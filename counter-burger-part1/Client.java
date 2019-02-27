56f0543b02f0b6fa16a89cc02861ee519e700ab5
pujakb <30911013+pujakb@users.noreply.github.com>
2019-02-26 22:23:53 -0800

    Add files via upload

---
+++ counter-burger-part1/Client.java
@@ -0,0 +1,18 @@
+ 
+
+public class Client {
+
+    public static void runTest()
+    {
+        Component theOrder = BuildOrder.getOrder() ;
+        theOrder.printDescription();
+    }
+    
+   public static void main( String [] args )
+    {
+        Client c = new Client() ;
+        c.runTest() ;
+    }    
+    
+}
+ 
